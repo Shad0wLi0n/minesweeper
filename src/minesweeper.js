8ae7ffdc80e26078b1ea2fa86a5e21925c14873a
Braia <sbraia@BRAIA173752ALS.home>
2017-09-26 10:19:38 -0400

    more progress

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -22,10 +22,19 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
 	
 	let numberOfBombsPlaced = 0;
 	while(numberOfBombsPlaced < numberOfBombs) {
-		let randomRowIndex = Math.floor( Math.random() );
-		let randomColumnIndex = Math.floor( Math.random() );
+		let randomRowIndex = Math.floor( Math.random() * numberOfRows );
+		let randomColumnIndex = Math.floor( Math.random() * numberOfColumns );
+		//Must use control flow to take into account bombs that are already placed and roll again
+		board[randomRowIndex][randomColumnIndex] = 'B';
+		numberOfBombsPlaced++;
 	}
 
 	return board;
+};
+
+const printBoard = board => {
+	board.map(row => {
+		row.join(' | ');
+	});
 };
 // console.log(generatePlayerBoard(3,4));
